be0505dea927e4fd4de3797d37ea8124db5f7858
Sk Soyeb Akhter <saleramesty2@gmail.com>
2021-11-12 21:09:26 +0530

    Nov 12, 2021: Added login, registration, password change, etc support with the help of dj-rest-auth package. Then configured the package to use JWT instead of default token. Next I will add social media authentication support

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include 
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')), 
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
